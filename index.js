86484c11d8a74a709aa2a7015e111eb2aa0c581c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-27 15:00:40 -0300

    01:09 - desestruturação

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const frutas = ["maçã", "mamão", "laranja", "banana"];
+const [dia, mes, ano, ...res] = [16, 04, 1993, 15, 10, 20];
 
 
-// for (let i = 1; i < frutas.length; i++) {
-//     const nomeFruta = frutas[i];
 
-//     const indiceFruta = frutas.indexOf(nomeFruta)
-
-//     console.log(`${indiceFruta}-${nomeFruta}`);
-// }
-
-
-// frutas.forEach(function (fruta, indice, frutas) {
-//     console.log(`${fruta}-${indice}`);
-//     // console.log(frutas);
-// });
-
-// for (const indice in frutas) {
-//     console.log(indice);
-// }
-
-for (const nome of frutas) {
-    console.log(nome);
-}
+console.log(dia, mes, ano);
+console.log(res);
