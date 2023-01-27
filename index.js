149557b096d254edb6f29d32eaff003fddb81ab0
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-27 15:04:32 -0300

    01:17 - Juntando todo array com o método Join

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-// const numeros = [5, 6, 3, 1, 4];
+const frutas = ["maçã", "mamão", "laranja", "banana"];
 
-// const resultado = numeros.reduce(function (acumulador, valorAtual) {
-//     console.log(acumulador, valorAtual);
-//     return acumulador + valorAtual;
-// }, 0);
-
-// console.log(resultado);
-
-const cursos = [
-    {
-        nome: 'JavaScript',
-        videos: 10,
-    },
-    {
-        nome: 'PHP',
-        videos: 3,
-    },
-    {
-        nome: 'PYTHON',
-        videos: 35,
-    }
-]
-
-const totalVideos = cursos.reduce((acumulador, curso) => acumulador + curso.videos, 0)
-
-console.log(`O total de vídeos é ${totalVideos}`);
+// console.log(frutas.join());
+// console.log(frutas.join(" "));
+// console.log(frutas.join(" - "));
+console.log(frutas.join(", "));
