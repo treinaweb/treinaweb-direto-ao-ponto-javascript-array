17185db4cacc29646bfcb36eb5fdae6b8a7050de
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-27 14:57:35 -0300

    01:02 - Como criar Arrays

---
+++ index.js
@@ -0,0 +1,14 @@
+// [120, "dwad", {}, [], function () { }]
+
+// Pelo construtor do Array
+
+// 1 argumento
+// const array = new Array(3);
+// [,,]
+// 2 ou mais argumento
+const array = new Array(3, 5);
+// console.log(array);
+
+// Pela notação literal
+const forma2 = [3];
+console.log(forma2);
