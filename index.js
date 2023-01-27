5a30eee1c7a00665af8636b4e7d0aa38a5a82470
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-27 14:59:27 -0300

    01:07 -  Buscando um item do Array com o método Find

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const frutas = ["maçã", "mamão", "laranja", "banana"];
-// 1 argumento -> acessa o indice do array
-// 2 argumento -> ele serve para remover o elemento
-// 3 ou N argumento -> serve para adicionar elementos
-// frutas.splice(2, 2, "melancia", "morango");
+// const numeros = [5, 12, 8, 130, 44];
 
-// Remova 0 elementos(zero) antes do índice 2 e insira "melancia"
-// frutas.splice(2, 0, "melancia");
+// const numero = numeros.find(function (numero) {
+//     return numero > 10;
+// });
 
-// Remova 1 elemento no índice 3]
-// frutas.splice(3, 1);
+// console.log(numero);
 
-//Remova 1 elemento no índice 2 e insira "melancia"
-// frutas.splice(2, 1, 'melancia');
+const alunos = [
+    {
+        nome: 'Ariel',
+        sobrenome: 'Sardinha'
+    },
+    {
+        nome: 'Elton',
+        sobrenome: 'Fonseca'
+    },
+    {
+        nome: 'Amauri',
+        sobrenome: 'Blanco'
+    }
+];
 
-// Remova 2 elementos do índice 0 e insira "melancia", "morango" e "coco"
-// frutas.splice(0, 2, "melancia", "morango", "coco");
+const aluno = alunos.find((aluno) => aluno.nome === 'Amauri');
 
-// Remova 2 elementos, começando no índice 2
-// frutas.splice(2, 2);
-
-// Remova 1 elemento do índice -2
-// frutas.splice(-2, 1);
-
-// Remova todos os elementos, começando no índice 2
-frutas.splice(2);
-
-console.log(frutas);
-
+console.log(aluno);
