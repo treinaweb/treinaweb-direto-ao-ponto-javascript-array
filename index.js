65208289d4d102d4973aced90f1d8ff6a37d0825
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-27 15:02:44 -0300

    01:15 - Filtrando um Array com o método Filter

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const animais = ['bufalo', 'camelo', 'pato', 'elefante'];
+const frutas = ["maçã", "mamão", "laranja", "banana", "pera"];
 
-const frutas = ["maçã", "mamão", "laranja", "banana"];
+const numeros = [10, 5, 30, 27, 18];
 
+const frutasFiltradas = frutas.filter(function (fruta, indice, frutas) {
+    return fruta.length <= 5
+});
 
-// const array = ["uva, pera"].concat(animais).concat(frutas);
+const numerosP = numeros.filter(function (numero) {
+    return numero % 2 === 0;
+});
 
-// const array = animais.concat(frutas);
+const numerosPares = numeros.map((numero) => {
+    if (numero % 2 === 0) {
+        return numero;
+    }
+});
 
-const array = animais.concat(["zebra, onça"]);
-
-console.log(array);
+console.log(numerosPares);
+console.log(numerosP);
